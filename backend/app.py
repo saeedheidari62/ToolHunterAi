0df9ecf40b15231737e2f3adba3a5eb1bf89eca0
saeedheidari62 <saeed.heidari62@gmail.com>
2026-07-28 09:55:03 +0330

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,10 @@
-print("ToolHunterAI started")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ToolHunterAI API is running"
+
+if __name__ == "__main__":
+    app.run()
